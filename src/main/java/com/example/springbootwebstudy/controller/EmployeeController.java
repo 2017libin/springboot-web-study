98492917f18224b10da40536fc15bcb722e1ea40
2017chase <1963482624@qq.com>
2023-12-30 03:47:53 +0800

    add addUser

---
+++ src/main/java/com/example/springbootwebstudy/controller/EmployeeController.java
@@ -0,0 +1,52 @@
+package com.example.springbootwebstudy.controller;
+
+import com.example.springbootwebstudy.dao.DepartmentDao;
+import com.example.springbootwebstudy.dao.EmployeeDao;
+import com.example.springbootwebstudy.pojo.Department;
+import com.example.springbootwebstudy.pojo.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+public class EmployeeController {
+    @Autowired
+    EmployeeDao employeeDao;
+
+    /**
+     * 查询所有员工，返回列表页面
+     * @param model
+     * @return
+     */
+    @RequestMapping("/emps")
+    public String list(Model model){
+        Collection<Employee> employees = employeeDao.getAll();
+        // 结果返回前端
+        model.addAttribute("emps",employees);
+        return "emp/list";
+    }
+
+    @Autowired
+    DepartmentDao departmentDao;
+
+    @GetMapping("/add")
+    public String add(Model model){
+        // 查出所有的部门信息
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments", departments);
+        return "emp/add";
+    }
+
+    @PostMapping("/add")
+    public String addEmp(Employee employee) {
+        System.out.println(employee);
+        // 添加员工
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+}

7eaf636d8a39bc01eab105821d9a27d5d1956455
2017chase <1963482624@qq.com>
2023-12-30 11:44:43 +0800

    add update_emp and delete_emp

--- src/main/java/com/example/springbootwebstudy/controller/EmployeeController.java
+++ src/main/java/com/example/springbootwebstudy/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -47,6 +48,31 @@ public class EmployeeController {
         System.out.println(employee);
         // 添加员工
         employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public String toUpdateAll(@PathVariable("id") int id, Model model) {
+        // 查询指定id的员工,添加到emp中,用于前端接收
+        Employee employee = employeeDao.getEmployee(id);
+        model.addAttribute("emp", employee);
+        // 查出所有的部门信息,添加到departments中,用于前端接收
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments", departments);
+        return "/emp/update";
+        // 返回到编辑员工页面
+    }
+
+    @PostMapping("/updateEmp")
+    public String updateEmp(Employee employee){
+        employeeDao.save(employee);
+        // 回到员工列表页面
+        return "redirect:/emps";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Integer id) {
+        employeeDao.delete(id);
         return "redirect:/emps";
     }
 }

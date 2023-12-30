63ba821ee63b6c6e8fee95d42138945afc81e1e8
2017chase <1963482624@qq.com>
2023-12-30 11:53:35 +0800

    add doc

--- src/main/java/com/example/springbootwebstudy/controller/LoginController.java
+++ src/main/java/com/example/springbootwebstudy/controller/LoginController.java
@@ -26,4 +26,10 @@ public class LoginController {
             return "index";
         }
     }
+
+    @RequestMapping("/user/loginOut")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/index.html";
+    }
 }
